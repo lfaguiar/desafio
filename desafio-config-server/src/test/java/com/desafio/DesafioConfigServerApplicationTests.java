5390a0494c37265d97cc92e11ad39412224d4aed
f729569 <f729569@corp.caixa.gov.br>
2018-03-23 18:22:16 -0300

    Correções

--- desafio-config-server/src/test/java/com/desafio/DesafioConfigServerApplicationTests.java
+++ desafio-config-server/src/test/java/com/desafio/DesafioConfigServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.coderef.delivery;
+package com.desafio;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DeliveryConfigServerApplicationTests {
+public class DesafioConfigServerApplicationTests {
 
 	@Test
 	public void contextLoads() {
--- desafio-eureka-server/src/test/java/com/desafio/DesafioEurekaServerApplicationTests.java
+++ desafio-eureka-server/src/test/java/com/desafio/DesafioEurekaServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.coderef.delivery;
+package com.desafio;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
