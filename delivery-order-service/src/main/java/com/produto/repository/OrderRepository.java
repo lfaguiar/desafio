764bdcf4211de46266146bd5e52df77800ed5c90
f729569 <f729569@corp.caixa.gov.br>
2018-03-23 17:27:28 -0300

    exclud

--- delivery-order-service/src/main/java/com/produto/repository/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package com.produto.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.produto.model.Order;
-
-public interface OrderRepository extends CrudRepository<Order, Integer> {
-}
